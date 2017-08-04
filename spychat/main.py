5c97f7dfdb4e976420b5c6fd4be80e1850a41741
anantmishra1997 <kanhamishra76@yahoo.in>
2017-08-04 12:33:23 +0530

    spychat application initiated

--- spychat/main.py
+++ spychat/main.py
@@ -1,1 +1,4 @@
 print 'Let\'s get started'
+spy_salutation=raw_input('What should we call you(Mr or Mrs) ?:')
+spy_name=raw_input('What is your name ?:')
+print 'Welcome '+spy_salutation+' '+spy_name+' glad to meet you'
