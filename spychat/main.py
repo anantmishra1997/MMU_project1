1482d60443ec0e49f2d6490c9a8f979bd7ff835b
anantmishra1997 <kanhamishra76@yahoo.in>
2017-08-09 12:02:33 +0530

    spychat python

--- spychat/main.py
+++ spychat/main.py
@@ -1,4 +1,8 @@
 print 'Let\'s get started'
 spy_salutation=raw_input('What should we call you(Mr or Mrs) ?:')
 spy_name=raw_input('What is your name ?:')
-print 'Welcome '+spy_salutation+' '+spy_name+' glad to meet you'
+
+if (len(spy_name)==0):
+    print "A spy needs a Proper Name"
+else:
+    print 'Welcome '+spy_salutation+' '+spy_name+' glad to meet you'
